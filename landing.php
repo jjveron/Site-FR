9aadac2c86000564982c56b960cd40f4d246c56c
HazardJ <jamesghazard@gmail.com>
2015-04-17 09:41:46 +0200

    Update landing.php

--- landing.php
+++ landing.php
@@ -12,15 +12,15 @@ include("header.php");
      <p align="center"><img src="assets/cmacc-trans.png" style="width:65%" /></p>
 <br/>
 <hr>
-<h1 class="title">un <a href="http://bitly.com/1DWGTxs">Code Civil 3.0</a></h1>
-<br/><br/>
-
-</div></div>
-<table><tr><td valign=top>
+<table><tr><td>
+<h1 class="title">un <a href="http://bitly.com/1DWGTxs">Code Civil 3.0</a></h1><br><br>
         <h4 class="subtitle"><a href=<?=$_SERVER['PHP_SELF']?>?action=list&file=/>Documents</a>.</h4>
-</td></tr></table>
 
-    </div> <!--end containerHome !-->
+</td><td>
+
+<a class="twitter-timeline" href="https://twitter.com/CommonAccord/with_replies" data-widget-id="574817616360964096">Tweets by @CommonAccord</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+</td></tr></table>
 
 
 </body><!-- end body !-->
