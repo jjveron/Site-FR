145d3e63ddfcb2d0d5a2cf79ab94fb9458f77546
HazardJ <jamesghazard@gmail.com>
2015-04-17 09:50:31 +0200

    Update landing.php

--- landing.php
+++ landing.php
@@ -13,14 +13,14 @@ include("header.php");
 <br/>
 <hr>
 <table><tr><td width="40%">
-<h1 class="title">un <a href="http://bitly.com/1DWGTxs">Code Civil 3.0</a></h1><br><br>
-        <h4 class="subtitle"><a href=<?=$_SERVER['PHP_SELF']?>?action=list&file=/>Documents</a>.</h4>
+<h2>un <a href="http://bitly.com/1DWGTxs">Code Civil 3.0</a></h2><br><br>
+        <h2 ><a href=<?=$_SERVER['PHP_SELF']?>?action=list&file=/>Documents</a>.</h2>
 
 </td><td> &emsp; 
 </td><td> &emsp; 
-</td><td width="25%">
+</td><td width="40%">
 
-<a class="twitter-timeline" href="https://twitter.com/CommonAccord/with_replies" data-widget-id="574817616360964096" width="400" height="300">Tweets by @CommonAccord</a>
+<a class="twitter-timeline" href="https://twitter.com/CommonAccord/with_replies" data-widget-id="574817616360964096" width="600" height="300">Tweets by @CommonAccord</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 </td></tr></table>
 
