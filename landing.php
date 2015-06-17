2fcc65cbb4767ff148f1ac490dde060cd8a70a29
James Hazard <james.g.hazard@gmail.com>
2015-06-17 13:28:39 +0200

    restore page d'accueil

--- landing.php
+++ landing.php
@@ -1,11 +1,28 @@
-     <p align="center"><img src="assets/cmacc-trans.png" style="width:35%" /></p>
+<?php
+/*require('./vendor/autoload.php'); */
+ini_set("allow_url_include", true);
+include("header.php");
 
-<table><tr><td>Welcome to a subdomain of CommonAccord.
-<br> Here: <a href="index.php?action=list&file=/">Documents</a>:<br><br>
-This is a subset of the docs from <a href="http://www.commonaccord.org">commonaccord.org</a>.<br>
+?>
 
-</td><td>
+<body>
+  <div class="container" id ="containerHome" style="padding:50px">
+    <div class="row">
+     <div class = "col-xs-12">
+     <p align="center"><img src="assets/cmacc-trans.png" style="width:65%" /></p>
+<br/>
+<hr>
+<table><tr><td width="40%">
+<h2>vers un <a href="http://bitly.com/1DWGTxs">Code Civil 3.0</a></h2><br><br>
+        <h2 ><a href=<?=$_SERVER['PHP_SELF']?>?action=list&file=/>Documents</a></h2>
+
+</td><td> &emsp; 
+</td><td> &emsp; 
+</td><td width="40%">
 
 <a class="twitter-timeline" href="https://twitter.com/CommonAccord/with_replies" data-widget-id="574817616360964096" width="600" height="300">Tweets by @CommonAccord</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 </td></tr></table>
+
+
+</body><!-- end body !-->
